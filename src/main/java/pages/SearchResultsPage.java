772fa33d5400973443d4b1af01d9c566da269c19
EMAN\EmanMustafa <emanmustafa2009@gmail.com>
2023-01-16 03:28:53 +0200

    edit

--- src/main/java/pages/SearchResultsPage.java
+++ src/main/java/pages/SearchResultsPage.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebDriver;
 public class SearchResultsPage {
     private WebDriver driver;
 
+    //**To Do--> get handler to this pages elements**//
+    //**To Do--> write methods to interact with elements**//
     public SearchResultsPage(WebDriver driver) {
         this.driver = driver;
     }
